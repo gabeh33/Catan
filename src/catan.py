af8fa9bbaed30b9de6dde78b51ccec3f465d7275
Gabe <gabeholmes1@gmail.com>
2021-11-28 16:21:18 -0500

    Resource distribution after rolling dice

--- src/catan.py
+++ src/catan.py
@@ -1,7 +1,8 @@
 # The classic Catan board game by Gabe Holmes, will make it multiplayer eventually
 # Right now focusing on playing against one bot
+import math
+import random
 from enum import Enum
-
 import pygame
 import os
 
@@ -15,7 +16,8 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (36, 0, 200)
 GRAY = (130, 130, 130)
-
+RED = (255, 0, 0)
+GREEN = (0, 180, 0)
 FPS = 60
 
 TILE_WIDTH, TILE_HEIGHT = 99.2, 111.32
@@ -54,25 +56,45 @@ legal_settlement_pos = [(320, 36), (415, 35), (516, 35), (273, 61), (368, 62), (
                         (612, 312), (221, 365), (320, 365), (417, 366), (513, 366), (612, 367),
                         (269, 394), (367, 393), (467, 391), (562, 394), (270, 449), (369, 448),
                         (466, 449), (564, 449), (319, 477), (417, 477), (513, 477)]
+settlement_circle_size = 10
+settlements_placed = []
+SETTLEMENT_WIDTH = 15
+SETTLEMENT_HEIGHT = 15
 
+red_settlement = pygame.image.load(os.path.join('Assets', 'Pieces', 'red_settlement.png'))
+red_settlement = pygame.transform.scale(red_settlement, (SETTLEMENT_WIDTH, SETTLEMENT_HEIGHT))
 
-def draw_circle(x_pos, y_pos):
-    pygame.draw.circle(WIN, GRAY, (x_pos, y_pos), 10)
+# The coordinates of all of the number tiles
+# In the board class we will map each number that the dice can roll to a coordinate here, and a resource
+# We then check if any settlements are within a certain distance from these coordinates, and if they
+# are we add the resource to their player.
+number_tile_pos = [(321, 89), (419, 88), (509, 87), (270, 173), (367, 176), (467, 172), (564, 173),
+                   (219, 256), (322, 253), (417, 251), (517, 254), (610, 252), (270, 340), (365, 339),
+                   (465, 340), (566, 340), (320, 422), (422, 421), (519, 420)]
 
 
 def place_starting_settlement():
-    # TODO draw circles everywhere settlements can be placed
+    # Draws circles where any starting settlement can be placed, and then waits for a choice from the player
     for pos in legal_settlement_pos:
-        draw_circle(pos[0], pos[1])
+        pygame.draw.circle(WIN, GRAY, (pos[0], pos[1]), settlement_circle_size)
+
+    # has_chosen is False until the player clicks in a legal position
 
 
 # Renders all of the info
-def draw_board(board, player, place_settlements):
+def draw_board(board, player, place_settlements, settlements_placed, total):
     WIN.fill(BLUE)
     board.draw_board()
     player.draw_hand()
     if place_settlements:
         place_starting_settlement()
+    for pos in settlements_placed:
+        pygame.draw.circle(WIN, RED, (pos[0], pos[1]), settlement_circle_size)
+    font = pygame.font.Font('freesansbold.ttf', 18)
+    roll_dice = font.render(str(total), True, BLACK, GREEN)
+    dice_small_rect = roll_dice.get_bounding_rect()
+    dice_small_rect.center = (60, 30)
+    WIN.blit(roll_dice, dice_small_rect)
     pygame.display.update()
 
 
@@ -84,6 +106,10 @@ class Board:
         # The inited list is used as a default
         self.resource_list = [WOOD, SHEEP, WHEAT, BRICK, ORE, BRICK, SHEEP,
                               DESERT, WOOD, WHEAT, WOOD, WHEAT, BRICK, SHEEP, SHEEP, ORE, ORE, WHEAT, WOOD]
+
+        self.resource_strings = ["WOOD", "SHEEP", "WHEAT", "BRICK", "ORE", "BRICK", "SHEEP",
+                                 "DESERT", "WOOD", "WHEAT", "WOOD", "WHEAT", "BRICK", "SHEEP", "SHEEP", "ORE", "ORE",
+                                 "WHEAT", "WOOD"]
 
         # Map from int -> list of ints where the first int is a number tile value [2, 12] and the second
         # is the indices in resource_list that the number tile maps to
@@ -124,6 +150,31 @@ class Board:
     def gen_random_board(self):
         # TODO Make this board generate a random assortment of tiles, but only allowing 4 of any one resource
         return
+
+    # Maps the possible numbers the dice can roll to a coordinate representing the resource tile position
+    # and a resource itself
+    def get_number_to_pos_and_resource_mapping(self):
+        mapping = {}
+        for i in range(2, 13):
+            if i != 7:
+                index_list = self.get_key_by_value(i)
+                mapping_list = []
+                for index in index_list:
+                    try:
+                        mapping_list.append((number_tile_pos[index], self.resource_strings[index]))
+                    except IndexError:
+                        print(str(i) + " index_list:")
+                        print(index_list)
+                mapping[i] = mapping_list
+        mapping[7] = ([], "DESERT")
+        return mapping
+
+    def get_key_by_value(self, value):
+        key_list = []
+        for key in self.number_tile_mapping:
+            if self.number_tile_mapping[key] == value:
+                key_list.append(key)
+        return key_list
 
     # Draws the hexagonal tiles, and then draws the number tiles on top
     def draw_board(self):
@@ -230,6 +281,26 @@ class Player:
             card = pygame.image.load(os.path.join('Assets', 'Resources', name + '_card.png'))
             self.resource_cards.append(pygame.transform.scale(card, (self.card_width, self.card_height)))
 
+    def update_resources(self, resource_string):
+        print(F"Resource massed = {resource_string}")
+        if resource_string == "WHEAT":
+            print("got wheat")
+            self.wheat += 1
+        elif resource_string == "BRICK":
+            print("got brick")
+            self.brick += 1
+        elif resource_string == "SHEEP":
+            print("got sheep")
+            self.sheep += 1
+        elif resource_string == "WOOD":
+            print("got wood")
+            self.wood += 1
+        elif resource_string == "ORE":
+            print("got ore")
+            self.ore += 1
+        else:
+            return
+
     def can_build_settlement(self):
         return self.wood and self.wheat and self.sheep and self.brick
 
@@ -297,22 +368,84 @@ class Player:
                        starting_y + 2 * self.card_height + vertical_buffer - 15)
         WIN.blit(num, rect)
 
+        # Draw the roll dice rectangle
+        font = pygame.font.Font('freesansbold.ttf', 18)
+        roll_dice = font.render('Roll Dice!', True, BLACK, GREEN)
+        dice_small_rect = roll_dice.get_bounding_rect()
+        dice_small_rect.center = (WIDTH - 60, HEIGHT - 30)
+        WIN.blit(roll_dice, dice_small_rect)
+        # Boundaries are width - 100, height - 36
+        # width - 17, height - 21
+
+
+# Checks if the coordinates are a valid settlement position
+# If it is draws a red circle and returns True, False otherwise
+def check_starting_settlement_placed(x_cord, y_cord):
+    for pos in legal_settlement_pos:
+        if math.hypot(pos[0] - x_cord, pos[1] - y_cord) <= settlement_circle_size:
+            settlements_placed.append(pos)
+            legal_settlement_pos.remove(pos)
+            # TODO remove the 2 or 3 adjacent settlements from legal_settlement_pos too
+            WIN.blit(red_settlement, (pos[0], pos[1]))
+            return True
+    return False
+
+
+def check_roll_dice_clicked_and_roll(x_cord, y_cord, board, player):
+    if WIDTH - 100 <= x_cord <= WIDTH - 17 and HEIGHT - 36 <= y_cord <= HEIGHT - 21:
+        return roll_dice_and_distribute(board, player)
+
+
+def get_distance(x1, y1, x2, y2):
+    asq = (x1 - x2) ** 2
+    bsq = (y1 - y2) ** 2
+    return math.sqrt(asq + bsq)
+
+
+def roll_dice_and_distribute(board, player):
+    num1 = random.randint(1, 6)
+    num2 = random.randint(1, 6)
+    total = num1 + num2
+    if total == 7:
+        print("Got a 7, returning...")
+        return 7
+    print(str(total))
+    # print(F"num1: {num1} num2: {num2} total: {total}")
+    # List of tuples that contain ((x_cord, y_cord), resource_tile)
+    mapping = board.get_number_to_pos_and_resource_mapping()
+    lst = mapping[total]
+    circle_radius = 76.02
+    for entry in lst:
+        if entry[1] != "DESERT":
+            for settlement_pos in settlements_placed:
+                if get_distance(settlement_pos[0], settlement_pos[1], entry[0][0],  entry[0][1]) <= circle_radius:
+                    player.update_resources(entry[1])
+    return total
 
 def main():
     board1 = Board()
     player1 = Player()
-    need_place_starting_settle = True
+    drew_settlement = False
+    draw_starting_settlements = True
+    pos = []
     run = True
+    total = -1
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
             if event.type == pygame.MOUSEBUTTONUP:
-                if need_place_starting_settle:
-                    need_place_starting_settle = False
+                x_cord, y_cord = pygame.mouse.get_pos()
+                #print(str(x_cord) + " " + str(y_cord))
+                pos.append((x_cord, y_cord))
+                drew_settlement = check_starting_settlement_placed(x_cord, y_cord)
+                total = check_roll_dice_clicked_and_roll(x_cord, y_cord, board1, player1)
 
-        draw_board(board1, player1, True)
+        if drew_settlement:
+            draw_starting_settlements = False
 
+        draw_board(board1, player1, draw_starting_settlements, settlements_placed, total)
+    # print(pos)
     pygame.quit()
 
 
